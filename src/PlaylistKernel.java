8f7bba71e4f57ce71aba8ee4040ad8c3549e8378
chetanschool <chetanschool3@gmail.com>
2025-10-25 11:38:19 -0400

    Update PlaylistKernel.java

--- src/PlaylistKernel.java
+++ src/PlaylistKernel.java
@@ -5,7 +5,7 @@ import components.standard.Standard;
  *            Types of songs in playlist
  */
 
-public interface PlaylistKernel<T> extends Standard<Playlist> {
+public interface PlaylistKernel<T> extends Standard {
 
     /**
      * Adds a song to playlist
@@ -15,5 +15,29 @@ public interface PlaylistKernel<T> extends Standard<Playlist> {
      * @updates this
      * @ensures this = #this * <s>
      */
+    void addSong(T s);
+
+    /**
+     * Removes song and returns song at index i
+     *
+     * @param index
+     *            position in song
+     * @requires 0<= index < |this|
+     * @updates this
+     * @ensures <pre>
+     * removes song at given index
+     * </pre>
+     *
+     * @return removed song
+     */
+    T removeSong(int index);
+
+    /**
+     * returns size of playlist
+     *
+     * @return size of playlist
+     * @ensures getSize = |this|
+     */
+    int getSize();
 
 }
